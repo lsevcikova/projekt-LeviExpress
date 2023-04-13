b45df7c09a1972f070409df960ce3a183bdef658
Lucka <48294719+lsevcikova@users.noreply.github.com>
2023-04-13 21:04:37 +0200

    fixed bug

--- src/components/Reservation/index.jsx
+++ src/components/Reservation/index.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { App } from './components/App';
-import { Reservation } from './components/Reservation';
-import './style.css';
-import { Home } from './components/Home';
 
-export const API_BASE_URL = 'http://leviexpress-backend.herokuapp.com/api';
-
-const ErrorPage = () => {
-    return (
-      <main className='container'>
-        <h1>Chyba 404: tady nic neni</h1>
-        <p>Pravdepodobne mate spatny odkaz.</p>
-      </main>
-    )
-  }
-
-const router = createBrowserRouter ([
-    {
-        path: '/',
-        element: <App />,
-        errorElement: <ErrorPage />,
-     
-    }
-])
-
-createRoot(document.querySelector('#app')).render(
-    <RouterProvider router={router} />
-  )
+export const Reservation = () => (
+  <>
+    <h2>Detail jízdenky</h2>
+  </>
+);
