b3793d527d8cc98e3f100e8509ef89b0ca3598b3
lsevcikova <48294719+lsevcikova@users.noreply.github.com>
2023-06-12 20:41:52 +0200

    added selected seat

--- src/components/Seat/index.jsx
+++ src/components/Seat/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './style.css';
 
-export const Seat = ({ number, isOccupied }) => (
+export const Seat = ({ number, isOccupied, isSelected}) => (
     <>
-        <svg class={ isOccupied ? 'seat seat--occupied' : 'seat' }  viewBox="0 0 100 100" role="button">
+        <svg class={ `seat ${isOccupied && 'seat--occupied' && isSelected && 'seat--selected'}`}  viewBox="0 0 100 100" role="button">
             <rect class="seat__rect" width="80" height="80" x="14" y="10" rx="15" ry="15" />
             <path class="seat__path" d="M 65,10 H 25 C 5,35 5,65 25,90 H 65" />
             <text class="seat__text" x="55" y="65">{number}</text>
--- src/components/SeatRow/index.jsx
+++ src/components/SeatRow/index.jsx
@@ -1,26 +1,11 @@
 import React from 'react';
 import { Seat } from '../Seat';
 
-const testRow = [
-    {
-      number: 33,
-      isOccupied: false,
-    },
-    {
-      number: 29,
-      isOccupied: true,
-    },
-    {
-      number: 25,
-      isOccupied: false,
-    },
-  ];
-
 export const SeatRow = ({ row }) => {
     return (
         <div className='seat-row'>
             {row.map(seat => (
-            <Seat number={seat.number} />
+            <Seat number={seat.number} isOccupied={seat.isOccupied}/>
             ))}
         </div>
     )
